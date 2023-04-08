024f24b7aa0d0dc63d25259027e740ec39fd6d8b
Арсений <arsenyzakharian2004@gmail.com>
2023-04-08 20:19:47 +0300

    added 10 class page

--- page1/urls.py
+++ page1/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('registration', views.registration, name='registration'),
-    path('login', views.login, name='login')
+    path('login', views.login, name='login'),
+    path('class10', views.class10, name='class10')
 ]
--- page1/views.py
+++ page1/views.py
@@ -8,4 +8,6 @@ def registration(request):
     return render(request, 'page1/registration.html') # представляем, что мы уже в templates
 def login(request):
     return render(request, 'page1/login.html') # представляем, что мы уже в templates
+def class10(request):
+    return render(request, 'page1/class10.html')
 # Create your views here.
