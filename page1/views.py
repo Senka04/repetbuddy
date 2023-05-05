dbc29e86c8fff960e2ce682a8d5015f1820d0c52
Senka04 <arsenyzakharian04@yandex.ru>
2023-05-05 14:40:29 +0300

    search (v2) - with subjects

--- page1/views.py
+++ page1/views.py
@@ -10,9 +10,11 @@ from django.urls import reverse
 from django.contrib import messages
 from .forms import VideoForm
 
+
 @login_required
 def confirm_logout(request):
     return render(request, 'page1/registration/confirm_logout.html')
+
 
 @login_required
 def logout_view(request):
@@ -21,12 +23,13 @@ def logout_view(request):
         return redirect('hello_page')
     else:
         return redirect(reverse('confirm_logout'))
+
+
 def login_view(request):
     # Проверяем, был ли уже выполнен вход на сайт
 
     if request.user.is_authenticated:
         return redirect('home')
-
 
     # Если это POST-запрос, обрабатываем данные формы
     if request.method == 'POST':
@@ -57,21 +60,31 @@ def register(request):
 
     return render(request, 'page1/registration/register.html', {'form': form})
 
+
 def tutor_main(request):
-    return render(request, 'page1/tutor_main.html') # представляем, что мы уже в templates
+    return render(request, 'page1/tutor_main.html')  # представляем, что мы уже в templates
+
+
 def user_main(request):
-    return render(request, 'page1/user_main.html') # представляем, что мы уже в templates
+    return render(request, 'page1/user_main.html')  # представляем, что мы уже в templates
+
+
 def hello_page(request):
     return render(request, 'page1/hello_page.html')
 
-def search(request, query=None):
-    query = request.GET['search_query']
-    try:
-        user_obj = TutorProfile.objects.filter(user__username=query)
-    except:
-        user_obj = TutorProfile.objects.none()
+
+def search(request, query="", subject=""):
+    query = request.GET.get('search_query')
+    subject = request.GET.get('subject')
+    user_obj = TutorProfile.objects.filter(user__username=query)
+    if query == "" and subject != "":
+        user_obj = TutorProfile.objects.filter(discipline=subject)
+    elif query != "" and subject != "":
+        user_obj = user_obj.filter(discipline=subject)
     params = {'user_obj': user_obj}
     return render(request, 'page1/search.html', params)
+
+
 def gohome(request):
     try:
         tutor_profile = TutorProfile.objects.get(user=request.user)
@@ -79,14 +92,21 @@ def gohome(request):
             return redirect('home_tutor')
     except TutorProfile.DoesNotExist:
         return redirect('home_user')
+
+
 def class9(request):
     return render(request, 'page1/class9.html')
+
+
 def class10(request):
     return render(request, 'page1/class10.html')
+
+
 def class11(request):
     return render(request, 'page1/class11.html')
-# Create your views here.
 
+
+# Create your views here.
 
 
 @login_required
@@ -108,6 +128,7 @@ def upload_video(request):
             form = VideoForm()
         return render(request, 'page1/videopleer/upload_video.html', {'form': form})
 
+
 @login_required
 def confirm_delete_video(request, pk):
     try:
@@ -118,6 +139,8 @@ def confirm_delete_video(request, pk):
         video = get_object_or_404(Video, pk=pk)
         if request.user == video.user:
             return render(request, 'page1/videopleer/confirm_delete_video.html', {'video': video})
+
+
 @login_required
 def delete_video(request, pk):
     try:
@@ -134,16 +157,18 @@ def delete_video(request, pk):
             messages.success(request, 'Video has been deleted successfully!')
             return redirect('home_tutor')
 
+
 @login_required
 def get_list_video(request):
     video_list = Video.objects.filter(user=request.user)
-    return render(request, 'page1/videopleer/home_tutor.html', {'video_list': video_list})
+    return render(request, 'page1/videopleer/home.html', {'video_list': video_list})
 
 
 @login_required
 def get_video(request, pk: int):
     _video = get_object_or_404(Video, id=pk, user=request.user)
     return render(request, "page1/videopleer/video.html", {"video": _video})
+
 
 @login_required
 def get_streaming_video(request, pk: int):
